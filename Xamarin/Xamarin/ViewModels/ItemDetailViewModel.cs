2583e1f2c7005a855182237caf458eafb837b0d9
Frederick MOTOT <frederick.motot2020@campus-eni.fr>
2020-09-28 16:33:31 +0200

    First commit TP1

---
+++ Xamarin/Xamarin/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Models;
+
+namespace Xamarin.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
